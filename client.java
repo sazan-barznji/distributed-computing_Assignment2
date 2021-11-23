6d956b48dd7ba4c9c16e341423a8830635fce969
sazan-barznji <barznjisazan99@yahoo.com>
2021-11-23 22:34:13 +0300

    new client with if statment

--- client.java
+++ client.java
@@ -14,8 +14,19 @@ public class client{
        Zookeeper zoo= new  Zookeeper("ipaddress",500,null);//input!! ip adress:portnumber, null== watcher
         //create znode 
         byte[] arr= toBytes("message");
-        zoo.create("/clients",null,ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);//null is the data
-        zoo.create("/clients/node1",arr,ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);//null is the data
+
+        if (zoo.exsists ("/clients",false)==null) {
+        zoo.create("/clients",null,ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);//null is the data	
+        }
+        if (zoo.exsist("/clients/node1",false)==null) {
+        zoo.create("/clients/node1",arr,ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);//null is the data	
+        }else {
+        	byte[]bs = zoo.getData("/clients/node1",false,null);
+        	String s = new String (bs,"UTF_8");
+        	System.out.println(s);
+        }
+        
+        
 
         connectedSignal.await();
    }
