1af65f3a3128e4737f5874422c73f1c2758bbb02
sazan-barznji <barznjisazan99@yahoo.com>
2021-11-23 22:23:01 +0300

    client

--- client.java
+++ client.java
@@ -7,8 +7,16 @@ import org.apache.zookeeper.data.*; //Stat
 import java.io.*;
 
 public class client{
+    public static byte[] toBytes (final String i) throws IOException{
+        return i.getBytes();
+    }
    public static void main(String[] args) throws Exception{
        Zookeeper zoo= new  Zookeeper("ipaddress",500,null);//input!! ip adress:portnumber, null== watcher
+        //create znode 
+        byte[] arr= toBytes("message");
+        zoo.create("/clients",null,ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);//null is the data
+        zoo.create("/clients/node1",arr,ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);//null is the data
 
+        connectedSignal.await();
    }
 }
